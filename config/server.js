67b029e52da06e9d59f506f4d68e9a18b7c1f0eb
kantimam <kantemir.imam@gmail.com>
2021-07-01 12:43:38 +0200

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c993920ae69498f09936cce9e6da3094'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '62983eda-a55f-4d09-b810-93438127195d'
+};
